eaf052784eca7d0d8325392344d9b94cb2232a1e
Nikhil Tej <149610100+Simpleitg@users.noreply.github.com>
2025-12-18 12:31:22 +0530

    Add files via upload

---
+++ exc.java
@@ -0,0 +1,22 @@
+class Demo {
+	void div() {
+		int n1 = 20;
+		int n2 = 0;
+		System.out.println(n1/n2);
+	}
+}
+class Example {
+
+	public static void main(String[] args) {
+		Demo d = new Demo();
+		try {
+			d.div();
+		}
+		catch(Exception e) {
+			e.getMessage();
+		}
+		
+
+	}
+
+}
