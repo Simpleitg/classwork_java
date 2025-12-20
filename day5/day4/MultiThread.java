df59ceb82043a6086d1c14d6a384c6f782874577
Nikhil Tej <149610100+Simpleitg@users.noreply.github.com>
2025-12-20 19:39:37 +0530

    Add files via upload

---
+++ day5/day4/MultiThread.java
@@ -0,0 +1,77 @@
+package day4;
+
+import java.util.Scanner;
+
+class Add implements Runnable
+{
+	public void run()
+	{
+		System.out.println("Addition");
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter First No");
+		int a = s.nextInt();
+		System.out.println("Enter Seconnd No");
+		int b = s.nextInt();
+		int sum = a+b;
+		System.out.println(sum);
+		System.out.println("Addition Task Completed....");
+		System.out.println("---------------------------------------");
+	}
+}
+
+		
+		
+class Cp implements Runnable
+{
+	public void run()
+	{
+		System.out.println("Character printing started");
+		for(int i=65;i<=75;i++)
+		{
+			System.out.println((char)i);
+			try {
+				Thread.sleep(1000);
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+class Np implements Runnable
+{
+	public void run()
+	{
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(i);
+			try {
+				Thread.sleep(1000);
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+public class MultiThread {
+
+	public static void main(String[] args) {
+		Add a = new Add();
+		Cp c = new Cp();
+		Np n = new Np();
+		
+		Thread t1 = new Thread(a);
+		Thread t2 = new Thread(c);
+		Thread t3 = new Thread(n);
+		
+		t1.start();
+		t2.start();
+		t3.start();
+	}
+
+}
